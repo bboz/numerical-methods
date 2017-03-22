cb92beb3f0e67305eb8a3574e6f995eed811be2c
Barış Boz <barisboz1997@gmail.com>
2017-03-22 16:35:01 +0200

    alt-ust-ucgenselmatris.py eklendi
    matrisi alt ucgensel ve ust ucgensel hale getirip çözme

---
+++ alt-ust-ucgenselmatris.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+import sys
+dosya=open("katsayilar.txt")
+matris = []
+
+for line in dosya.readlines():
+    line = line.rstrip('\n').split(' ')
+    matris.append(line)
+dosya.close()
+boyut = len(matris)
+
+for n in range(boyut):
+    kat = int(matris[n][n])
+    if(kat==0):
+        index=1
+        while(kat==0 and index+n<boyut):
+            matris[n],matris[n+index]=matris[n+index],matris[n]
+            index=index+1
+            kat = int(matris[n][n])
+        if(kat==0):
+            print("En az 2 satır birbirine bağlı birbirinden turemis sonuc yok ...")
+            sys.exit()
+    for m in range(boyut+1):
+        matris[n][m]=int(matris[n][m])/kat
+    for p in range(1,boyut-n):
+        kat = float(matris[n+p][n])
+        for q in range(boyut+1):
+            matris[n+p][q]=float(matris[n+p][q])-float(matris[n][q])*(kat/float(matris[n][n]))
+
+print(matris)
+
+for n in range(boyut):
+    kat = float(matris[boyut-n-1][boyut-n-1])
+    for m in range(boyut+1):
+        matris[boyut-n-1][boyut-m]=float(matris[boyut-n-1][boyut-m])/kat
+    for p in range(1,boyut-n):
+        kat=float(matris[boyut-n-1-p][boyut-n-1])
+        for q in range(boyut+1):
+            matris[boyut-n-1-p][q]=float(matris[boyut-n-1-p][q])-float(matris[boyut-n-1][q])*(kat/float(matris[boyut-n-1][boyut-n-1]))
+
+print(matris)
+for i in range(boyut):
+    print("x",i+1,"=",matris[i][boyut])
